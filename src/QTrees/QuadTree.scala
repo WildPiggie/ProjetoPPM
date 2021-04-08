d7ab5915013f4efd0878a98eb7c1d0e8f49e2975
pombosilva <olgasilva1904@gmail.com>
2021-04-08 14:59:48 +0100

    Criação da função mirrorV e início da criação da função mapColourEffect

--- src/QTrees/QuadTree.scala
+++ src/QTrees/QuadTree.scala
@@ -5,7 +5,7 @@ import QTrees.QuadTree._
 
 case class QuadTree(qt: QTree[Coords]){
 
-  def makeBitMap (): BitMap = QuadTree.makeBitMap(this.qt)
+  def makeBitMap (quadT: QTree[Coords]): BitMap = QuadTree.makeBitMap(qt) //correto?
   def scale (scale:Double, quadT:QTree[Coords]):QTree[Coords] = ???
   def mirrorV ():QTree[Coords] = QuadTree.mirrorV(this.qt)
   def mirrorH (quadT:QTree[Coords]):QTree[Coords] = ???
@@ -22,41 +22,74 @@ object QuadTree{
   type Section = (Coords, Color)
 
   def makeBitMap (qt: QTree[Coords]): BitMap = ???
-  def scale (scale:Double, qt:QTree[Coords]): QTree[Coords] = ??? //verificar se é possível realizar o scaling ou não
-  def mirrorV (qt:QTree[Coords]):QTree[Coords] = {
+  def scale (scale:Double, qt:QTree[Coords]):QTree[Coords] = ???
 
+  def mirrorV (qt:QTree[Coords]):QTree[Coords] = {
     qt match {
-      case (ql: QLeaf[Coords, Section]) => return qt
       case (qn: QNode[Coords]) => {
-        val t1 = mirrorV(qn.one)
-        val t2 = mirrorV(qn.two)
-        val t3 = mirrorV(qn.three)
-        val t4 = mirrorV(qn.four)
-        val (one, three) = switchQTrees(t1,t3)
-        val (two, four) = switchQTrees(t2,t4)
-        QNode( qn.value, three, four, one, two)
+        val height = qn.value._2._2 - qn.value._1._2
+        auxMirrorV(qn, height)
       }
     }
   }
+
+  def auxMirrorV(qt:QTree[Coords], height: Int): QTree[Coords] = {
+    qt match {
+      case (ql: QLeaf[Coords, Section]) => qt
+      case (qn: QNode[Coords]) => {
+        val (t1, t2, t3, t4) = (auxMirrorV(qn.one, height), auxMirrorV(qn.two, height), auxMirrorV(qn.three, height), auxMirrorV(qn.four, height))
+        val (one, two, three, four) = (updateCoordsV(t1, height), updateCoordsV(t2, height), updateCoordsV(t3, height), updateCoordsV(t4, height))
+        QNode(qn.value, three, four, one, two)
+      }
+    }
+  }
+
+  def updateCoordsV(qt: QTree[Coords], height: Int): QTree[Coords] = {
+    qt match {
+      case (qn:QNode[Coords]) =>
+        QNode( ((qn.value._1._1, height-qn.value._2._2), (qn.value._2._1, height-qn.value._1._2)), qn.one, qn.two, qn.three, qn.four)
+      case (ql:QLeaf[Coords, Section]) => {
+        QLeaf( (  ((ql.value._1._1._1, height-ql.value._1._2._2), (ql.value._1._2._1, height-ql.value._1._1._2))  , ql.value._2 ))
+      }
+    }
+  }
+
+  def samePoints(a: Point, b: Point): Boolean = (a._1==b._1) && (a._2==b._2)
+
+
   def mirrorH (qt:QTree[Coords]):QTree[Coords] = ???
   def rotateD (qt:QTree[Coords]):QTree[Coords] = ???
   def rotateR (qt:QTree[Coords]):QTree[Coords] = ???
-  def mapColourEffect (f:Color => Color, qt:QTree[Coords]):QTree[Coords] = ???
 
-  def switchQTrees(qt1: QTree[Coords], qt2: QTree[Coords]): (QTree[Coords], QTree[Coords]) = {
-    (qt1,qt2) match {
-      case (qn1:QNode[Coords], qn2:QNode[Coords]) =>
-        ( QNode(qn2.value, qn1.one, qn1.two, qn1.three, qn1.four), QNode(qn1.value, qn2.one, qn2.two, qn2.three, qn2.four) )
-      case (qn:QNode[Coords], ql:QLeaf[Coords, Section]) =>
-        ( QNode(ql.value._1, qn.one, qn.two, qn.three, qn.four), QLeaf( (qn.value,ql.value._2) ))
-      case (ql:QLeaf[Coords, Section], qn:QNode[Coords]) =>
-        ( QLeaf( (qn.value,ql.value._2) ), QNode(ql.value._1, qn.one, qn.two, qn.three, qn.four) )
-      case (ql1:QLeaf[Coords, Section], ql2:QLeaf[Coords, Section]) =>
-        ( QLeaf( (ql2.value._1, ql1.value._2) ) , QLeaf( (ql1.value._1, ql2.value._2) ) )
-      //case (q1, QEmpty) => (QEmpty, q1)   adicionar casos :)
-      case _ => (QEmpty, QEmpty)
-    }
+
+  def mapColourEffect (f:Color => Color, qt:QTree[Coords]):QTree[Coords] = {
+        qt match {
+          case ql: QLeaf[Coords, Section] => QLeaf((ql.value._1, f(ql.value._2)))
+          case qn: QNode[Coords] => {
+            mapColourEffect(f, qn.one)
+            mapColourEffect(f, qn.two)
+            mapColourEffect(f, qn.three)
+            mapColourEffect(f, qn.four)
+          }
+        }
   }
+
+  def noiseEffect (color: Color): Color ={
+    ???
+  }
+
+  def contrastEffect (color: Color): Color ={
+    ???
+  }
+
+  def sepiaEffect (color: Color): Color ={
+    val red= Math.min((.393 * color.getRed) + (.769 * color.getGreen) + (.189 * color.getBlue), 255.0).toInt
+    val green= Math.min((.349 * color.getRed) + (.686 * color.getGreen) + (.168 * color.getBlue), 255.0).toInt
+    val blue= Math.min((.272 * color.getRed) + (.534 * color.getGreen) + (.131 * color.getBlue), 255.0).toInt
+    new Color(red, green, blue)
+  }
+
+
 }
 
 
